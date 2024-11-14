f504aeb781281000e3d229cd6f50e5a99cd59995
mdiallo-fula <83314395+mdiallo-fula@users.noreply.github.com>
2024-11-14 18:19:04 -0500

    Add files via upload

--- navBar.js
+++ navBar.js
@@ -3,30 +3,27 @@
 // JavaScript code to inject the header
 document.addEventListener("DOMContentLoaded", function () {
   const navBar = `
-<nav class="navbar navbar-expand navbar-dark bg-dark fixed-top" aria-label="navbar">
-  <div class="container-fluid">
-    <h2 class="text-white">ACAWRA</h2>
-    <div class="collapse navbar-collapse" id="navbarLinks">
-      <ul class="navbar-nav me-auto" style="overflow-x:scroll">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="index.html"
-            >STEM Program</a
-          >
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="volunteer.html"
-            >Volunteer</a
-          >
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="parent.html"
-            >Register</a
-          >
-        </li>
-      </ul>
+<nav class="navbar navbar-expand-lg bg-dark border-bottom border-body fixed-top" data-bs-theme="dark">
+    <div class="container-fluid">
+      <h2 class="text-white">ACAWRA</h2>
+      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse" id="navbarColor01">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.html">STEM Program</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="volunteer.html">Volunteer</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="parent.html">Register</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
     `;
   // Inject header content into the body
   document.body.insertAdjacentHTML("afterbegin", navBar);
